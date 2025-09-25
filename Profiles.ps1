0e60234ed746d687bd3ece0ec25e658fbcb0882d
Pythonch1ck <151184912+Pythonch1ck@users.noreply.github.com>
2025-09-25 23:48:37 +0300

    Update Profiles.ps1

--- Profiles.ps1
+++ Profiles.ps1
@@ -1,10 +1,10 @@
 $adminPass = ConvertTo-SecureString "123456@gl" -AsPlainText -Force
 New-LocalUser "InTech_Admin" -Password $adminPass
-Add-LocalGroupMember -Group "Administrators" -Member "InTech_Admin"
+Add-LocalGroupMember -Group "S-1-5-32-544" -Member "InTech_Admin"
 
 $userPass = ConvertTo-SecureString "7654321" -AsPlainText -Force
 New-LocalUser "InTech" -Password $userPass
-Add-LocalGroupMember -Group "Users" -Member "InTech"
+Add-LocalGroupMember -Group "S-1-5-32-545" -Member "InTech"
 
 $SID = (Get-LocalUser InTech).SID
 $regPath = "Registry::HKEY_USERS\$SID\Software\Microsoft\Windows\CurrentVersion\Policies"
