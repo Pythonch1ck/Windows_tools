4da03df466d6d5f79c13ab7a1d9c065e5f168a6c
Pythonch1ck <151184912+Pythonch1ck@users.noreply.github.com>
2025-09-25 23:31:12 +0300

    Create Profiles.ps1

---
+++ Profiles.ps1
@@ -0,0 +1,17 @@
+$adminPass = ConvertTo-SecureString "123456@gl" -AsPlainText -Force
+New-LocalUser "InTech_Admin" -Password $adminPass
+Add-LocalGroupMember -Group "Администраторы" -Member "InTech_Admin"
+
+$userPass = ConvertTo-SecureString "7654321" -AsPlainText -Force
+New-LocalUser "InTech" -Password $userPass
+Add-LocalGroupMember -Group "Пользователи" -Member "InTech"
+
+$SID = (Get-LocalUser InTech).SID
+$regPath = "Registry::HKEY_USERS\$SID\Software\Microsoft\Windows\CurrentVersion\Policies"
+
+New-Item -Path "$regPath\System" -Force | Out-Null
+New-ItemProperty -Path "$regPath\System" -Name "NoDispBackgroundPage" -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path "$regPath\System" -Name "NoControlPanel" -Value 1 -PropertyType DWord -Force
+
+New-Item -Path "$regPath\Explorer" -Force | Out-Null
+New-ItemProperty -Path "$regPath\Explorer" -Name "NoFileAssociate" -Value 1 -PropertyType DWord -Force
