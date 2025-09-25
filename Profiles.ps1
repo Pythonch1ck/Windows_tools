470b8b73de9211798da4c86b9c310f0a443298e8
Pythonch1ck <151184912+Pythonch1ck@users.noreply.github.com>
2025-09-25 23:57:57 +0300

    Update Profiles.ps1

--- Profiles.ps1
+++ Profiles.ps1
@@ -1,17 +1,28 @@
+# Переключаем консоль в UTF-8, чтобы русские символы не превращались в иероглифы
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+chcp 65001 | Out-Null
+
+# Создаём админа
 $adminPass = ConvertTo-SecureString "123456@gl" -AsPlainText -Force
 New-LocalUser "InTech_Admin" -Password $adminPass
-Add-LocalGroupMember -Group "S-1-5-32-544" -Member "InTech_Admin"
+Add-LocalGroupMember -Group "Администраторы" -Member "InTech_Admin"
 
+# Создаём обычного пользователя
 $userPass = ConvertTo-SecureString "7654321" -AsPlainText -Force
 New-LocalUser "InTech" -Password $userPass
-Add-LocalGroupMember -Group "S-1-5-32-545" -Member "InTech"
+Add-LocalGroupMember -Group "Пользователи" -Member "InTech"
 
+# SID профиля InTech
 $SID = (Get-LocalUser InTech).SID
 $regPath = "Registry::HKEY_USERS\$SID\Software\Microsoft\Windows\CurrentVersion\Policies"
 
+# Запрет изменения фона рабочего стола
 New-Item -Path "$regPath\System" -Force | Out-Null
 New-ItemProperty -Path "$regPath\System" -Name "NoDispBackgroundPage" -Value 1 -PropertyType DWord -Force
+
+# Запрет панели управления и параметров
 New-ItemProperty -Path "$regPath\System" -Name "NoControlPanel" -Value 1 -PropertyType DWord -Force
 
+# Запрет изменения ассоциаций файлов (ограничение скачивания и установки софта)
 New-Item -Path "$regPath\Explorer" -Force | Out-Null
 New-ItemProperty -Path "$regPath\Explorer" -Name "NoFileAssociate" -Value 1 -PropertyType DWord -Force
